fbaffa2ca22d021a318155f9dacb6b03f4e48db7
James Glenn <47917431+JR-G@users.noreply.github.com>
2024-09-10 16:54:33 +0100

    Further project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Endthon
+
+Endthon is a Python variant that introduces explicit `end` statements to denote the closing of code blocks, similar to languages like Ruby.
+
+## Installation
+
+```
+pip install -e .
+```
+
+## Usage
+
+TBC
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="endthon",
+        version="0.1.0",
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        install_requires=[],
+        entry_points={
+            "console_scripts": [
+                "endthon=endthon.runner:main",
+                ],
+        },
+        author="James Glenn",
+        author_email="",
+        description="Python but with explicit end",
+        long_description=oppn("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/JR-G/endthon",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        python_requires=">=3.6",
+        )
